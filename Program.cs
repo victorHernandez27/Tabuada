3e5d1848e9a002b85f2c5dd01c313af8dac22402
Victor Hernández <victor.hhernandez27@gmail.com>
2022-03-07 15:16:00 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Tabuada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 0 ;
+
+            do
+            {
+                Console.WriteLine("Digite um numero: ");
+                numero = Convert.ToInt32(Console.ReadLine());
+
+                if(numero < 1 ||numero > 9)
+                {
+                    Console.WriteLine("Numero Inválido");
+                }
+
+            } while (numero < 1 || numero > 9);
+
+            Console.WriteLine("Tabuado do: {0}", numero);
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine("{0} * {1} = {2}", numero,i,(numero*i));
+            }
+            Console.ReadKey();
+        }
+    }
+}
